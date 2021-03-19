627f76491ec393dd9125801a2f7aba895f064bc7
bartoszlomiej <jet.samb@gmail.com>
2021-03-19 19:02:15 +0100

    Trying to change name even if no extention is given

--- lab1/modify.sh
+++ lab1/modify.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
 option="non empty"
-recursion="False"
+recursion="-maxdepth 0"
 while getopts :lur flag #problem -> it is possible to make -lu flag! which is an error
 do
     case "${flag}" in #remember to make it impossible to choose -lu
-	l) option="[:lower:]" ;; #performing lowering the letter
-	u) option="[:upper:]" ;; #performing uppering the letter
-	r) recursion="True" ;; #performing l/u recursively
+	l) option="[:upper:] [:lower:]";; #performing lowering the letter
+	u) option="[:lower:] [:upper:]";; #performing uppering the letter
+	r) recursion="-depth" ;; #performing l/u recursively
     esac
 done
 
-echo $option
-echo $recursion
+#echo $option
+#echo $recursion
 
+#find -depth -type f //this command is used for finding files recursively
+
+#find -depth -type f -exec tr $option {} \; #to change files names there is a need to use mv!
+
+find $recursion -type f | tr $option
+
+traverse_directories () {
+	for f in *; do
+		if [ -d "$f" ]; then
+			echo $f #tutaj jest directory
+		else
+			filename=`echo $f | cut -f1 -d '.' #| tr $option`
+			fileextension=`echo ${f##*.}  #| cut -f2 -d '.'` 
+			echo $filename'.'$fileextension
+			#mv $f $filename
+		fi
+	done
+}
+
+traverse_directories
+
+#important TODOS: 1) get to know how to list all files in that way, that it is possible to use tr command, and further use the mv command (mv oldname NEWNAME), where NEWNAME is the output of the tr, and oldname is input to tr
