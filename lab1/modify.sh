edd5ff4aed01ff49e854ec68baa3e11f9ac128b9
bartoszlomiej <jet.samb@gmail.com>
2021-03-21 14:05:20 +0100

    cleaning was performed

--- lab1/modify.sh
+++ lab1/modify.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 option="non empty"
-recursion="-maxdepth 0"
+recursion="False"
 while getopts :lur flag #problem -> it is possible to make -lu flag! which is an error
 do
     case "${flag}" in #remember to make it impossible to choose -lu
 	l) option="[:upper:] [:lower:]";; #performing lowering the letter
 	u) option="[:lower:] [:upper:]";; #performing uppering the letter
-	r) recursion="-depth" ;; #performing l/u recursively
+	r) recursion="True" ;; #performing l/u recursively
     esac
 done
 
@@ -17,37 +17,15 @@ done
 
 #find $recursion -type f | tr $option
 
-traverse_directories () {
-	for f in *; do
-		if [ -d "$f" ]; then
-			echo $f #tutaj jest directory
-		else
-			filename=`echo $f | cut -f1 -d '.'` #| tr $option`
-			fileextension=`echo ${f##*.}`  #| cut -f2 -d '.'` 
-			if [[ $filename == $fileextension ]]; then
-				fileextension=""
-				dot=""
-			else
-				dot="."
-			fi
-			filename=`echo $f | cut -f1 -d '.' | tr $option`
-
-			newname=$filename$dot$fileextension
-			mv $f $newname
-		fi
-	done
-}
-
-#traverse_directories
-
 traverse () {
 	local a file #a is the first argument of a function. The a is used instead of $1 as it is impossible to iterate over $1
 	for a; do
 		for file in "$a"/*; do
 			if [[ -d $file ]]; then
-				traverse "$file"
+				if [[ $recursion == True ]]; then
+					traverse "$file"
+				fi
 			else
-		#		basename $file
 				filename=`basename $file | cut -f1 -d '.'` #| tr $option`
 				fileextension=`basename ${file##*.}`  #| cut -f2 -d '.'` 
 				if [[ $filename == $fileextension ]]; then
@@ -65,5 +43,3 @@ traverse () {
 	done
 }
 traverse `pwd` 
-
-#important TODOS: 1) get to know how to list all files in that way, that it is possible to use tr command, and further use the mv command (mv oldname NEWNAME), where NEWNAME is the output of the tr, and oldname is input to tr
