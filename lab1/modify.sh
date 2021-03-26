e56004a8ace9364bde1a2c398fe8e19940d6acbf
bartoszlomiej <jet.samb@gmail.com>
2021-03-26 20:14:16 +0100

    working on directe usage with files

--- lab1/modify.sh
+++ lab1/modify.sh
@@ -3,6 +3,10 @@
 option="non empty"
 recursion="False"
 
+
+#important: use the ${file##*.} to get the file extension!!!
+
+
 Help() {
 	echo "This program modifies the file names to upper/lower letters."
 	echo "Usage of flags:"
@@ -30,6 +34,25 @@ done
 
 #find $recursion -type f | tr $option
 
+change_name() {
+	filename=`basename $1 | cut -f1 -d '.'` #| tr $option`
+	fileextension=`basename ${1##*.}`  #| cut -f2 -d '.'`
+       	echo "extension: $fileextension"
+	echo "filename: $filename"
+	if [[ $filename == $fileextension ]]; then
+		fileextension=""
+		dot=""
+	else
+		dot="."
+	fi
+	filename=`basename $1 | cut -f1 -d '.' | tr $option`
+
+	newname=$filename$dot$fileextension
+	if [[ `basename $1` != $newname ]]; then
+		mv $2/$1 $2/$newname #here is the problem for now
+	fi
+}
+
 traverse () {
 	local a file #a is the first argument of a function. The a is used instead of $1 as it is impossible to iterate over $1
 	for a; do
@@ -39,22 +62,34 @@ traverse () {
 					traverse "$file"
 				fi
 			else
-				filename=`basename $file | cut -f1 -d '.'` #| tr $option`
-				fileextension=`basename ${file##*.}`  #| cut -f2 -d '.'` 
-				if [[ $filename == $fileextension ]]; then
-					fileextension=""
-					dot=""
-				else
-					dot="."
-				fi
-				filename=`basename $file | cut -f1 -d '.' | tr $option`
-	
-				newname=$filename$dot$fileextension
-				if [[ `basename $file` != $newname ]]; then
-					mv $a/`basename $file` $a/$newname
-				fi
+				onlyname=`readlink -f $file`
+				change_name $onlyname $a
 			fi
 		done
 	done
 }
-traverse `pwd` 
+#traverse `pwd`
+
+while test "$1" != ""
+do
+	echo "parameters: $1"
+	if [ -d "$1" ]; then
+		echo "dziala $1"
+	fi
+	if [ -f "$1" ]; then
+		pwd
+		change_name $1 `pwd`
+	fi	
+	shift
+done
+#todo:
+#0) give the directory/file names directly to lower/upper
+#1) sed
+#2) fix getopts
+#3) 
+
+#How to do the #0?
+#solution: -read all commandline parameters. Check if it is a file or directory. if file => apply the function lower/upper. If directory => function traverse 'directory path' (directory path = `pwd`/directoryname) 
+
+#current issue: when the file is test/Misiek.txt then mv tries to mv `pwd`/Misiek.tx instead of `pwd`/test/Misiek.txt to `pwd`/MISIEK.txt instead of `pwd`/test/MISIEK.txt
+
