9308e7bde9e09c6d271e5aa3b942f42dffc48e96
Bartłomiej Piotr Mastej <b.mastej@stud.elka.pw.edu.pl>
2021-04-09 17:56:46 +0200

    Initial commit

---
+++ lab2/tsig.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+/*
+ * create the NUM_CHILD processes. The NUM_CHILD must be defined in the program
+ * use the fork function
+ * insert one second delays between consecutive fork() calls.
+ */
+
+int main(){
+
+}
+
+
+
