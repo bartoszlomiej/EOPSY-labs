7d9b53f1e120ccb2b341264a80f407d0883ed350
Bartłomiej Piotr Mastej <b.mastej@stud.elka.pw.edu.pl>
2021-04-13 01:35:12 +0200

    Another approach was taken. Proper creation of NUM_CHILD number of processes. Unproper killing of the children

--- lab2/tsig.c
+++ lab2/tsig.c
@@ -7,34 +7,50 @@
 
 
 /*
- * 
- * 
- * 
+ * OBJECTIVE:
+ * 1) Kill children properly
+ * 2) Write the success after creation of all processes successfully
+ * 3) Exit with error code 1 if child was not created properly
  */
 
-void NUM_CHILD(){
-  printf("Hello World!\n");
+#define NUM_CHILD 5 //defined number of child processes
+
+/*
+void handle_sigchld(int sig){
+  printf("STH%d\n", sig); //just for the future purpouse of using handling
+}
+*/
+
+void kill_children(int* t){
+  for(int i = 1; i < NUM_CHILD; i++)
+    kill(t[i], SIGTERM); //terminates all processes apart from the oldest one.
 }
 
-void parent(){
-  int success = 0;
-  success = fork();
-  if( success == -1 ){
-    //call the SIGTERM signal here    
-    printf("The child proccess was not created properly");
-    exit(1);
+void create_processes(int* t){
+  t[0] = getpid(); //pid of the oldest process
+  for(int i = 1; i < NUM_CHILD; i++ ){
+    if(t[0] == getpid()){ //the processes will be created ONLY by the one (the oldest) processp
+      int code = fork();
+      if(code == -1){//optionally -> check if all codes are equal to 0 -> call the success.
+	kill_children(t);
+	printf("The child proccess was not created properly");
+	exit(1);
+      }
+      sleep(1);
+    }
   }
-  printf("Process: %d\n", success);
-  NUM_CHILD();
-  sleep(1);
 }
 
 int main(){
-  //  NUM_CHILD();
-  parent();
-  parent();
+  int t[NUM_CHILD]; //the table with all processes ID's
+  create_processes(t);
+  printf("ala ma kota\n");
+  if(t[0] == getpid()){
+    printf("All %d processes were created successfully", NUM_CHILD);
+    sleep(3);
+    kill_children(t);
+  }
+
+  //  while(1);
   return 0;
 }
-
-
-
