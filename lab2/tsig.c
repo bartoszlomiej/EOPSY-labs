e551376ea818c10a778eeaaa535474f3f1d83bcc
Bartłomiej Piotr Mastej <b.mastej@stud.elka.pw.edu.pl>
2021-04-13 22:00:53 +0200

    Proper way of waiting was implemented.

--- lab2/tsig.c
+++ lab2/tsig.c
@@ -9,9 +9,9 @@
 
 /*
  * OBJECTIVE:
- * 1) Call wait() function in the loop -> until receiving from system of all synchronized processes with the parent
- * 2) Print a message that there are no more child processes
- * 3) In this  loop do count child processes terminations and, at the very end of the  parent process, print a message with the number of just received child  processes exit codes
+ * 1) Call wait() function in the loop -> until receiving from system of all synchronized processes with the parent -- OK!
+ * 2) Print a message that there are no more child processes -- OK!
+ * 3) In this  loop do count child processes terminations and, at the very end of the parent process, print a message with the number of just received child  processes exit codes
  */
 
 #define NUM_CHILD 5 //defined number of child processes
@@ -39,7 +39,7 @@ void create_processes(int* t){
   for(int i = 1; i < NUM_CHILD; i++ ){
     if(t[0] == getpid()){ //the processes will be created ONLY by the one (the oldest) processp
       t[i] = fork();
-      wait(NULL);
+
       if(t[i] == -1){//optionally -> check if all codes are equal to 0 -> call the success.
 	kill_children(t);
 	printf("The child proccess was not created properly");
@@ -54,14 +54,18 @@ int main(){
   int t[NUM_CHILD]; //the table with all processes ID's
 
   create_processes(t);
-  printf("ala ma kota. Process PID: %d\n", getpid());
   
   if(t[0] == getpid()){
+    int i = 1;
+    while(i != NUM_CHILD){ //counting all children processes.
+      if(wait(NULL) != -1) //waiting for all children to die.
+	i = i + 1;
+    }
     printf("All %d processes were created successfully\n", NUM_CHILD);
     printf("The children are dead.\n");
     //    kill_children(t);
   }
-
+  printf("ala ma kota. Process PID: %d\n", getpid());
   //while(1);
   return 0;
 }
