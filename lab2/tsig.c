7d4024906d80bb831a8f7e2ee3c4082185e78e3e
bartoszlomiej <jet.samb@gmail.com>
2021-04-16 18:24:41 +0200

    interrupt handler

--- lab2/tsig.c
+++ lab2/tsig.c
@@ -3,60 +3,42 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-//#include <wait.h>
 #include <signal.h>
 
 
-/*
- * OBJECTIVE:
- * 1) Call wait() function in the loop -> until receiving from system of all synchronized processes with the parent -- OK!
- * 2) Print a message that there are no more child processes -- OK!
- * 3) In this  loop do count child processes terminations and, at the very end of the parent process, print a message with the number of just received child  processes exit codes
- */
-
 #define NUM_CHILD 5 //defined number of child processes
 
-/*
-void handle_sigchld(int sig){
-  printf("STH%d\n", sig); //just for the future purpouse of using handling
-}
-*/
-
-void synchronize(int status){
-  wait(&status);
-  if(WIFSIGNALED(status))
-    printf("Wow, sth have happened. PID: %d\n", getpid());
-}
+int mark = 0;
 
 void kill_children(int* t){
   for(int i = 1; i < NUM_CHILD; i++){
-    kill(t[i], SIGTERM); //terminates all processes apart from the oldest one.
+    if(t[i] > 0)
+      kill(t[i], SIGTERM); //terminates all processes apart from the oldest one.
   }
 }
 
 void create_processes(int* t){
   t[0] = getpid(); //pid of the oldest process
   for(int i = 1; i < NUM_CHILD; i++ ){
-    if(t[0] == getpid()){ //the processes will be created ONLY by the one (the oldest) processp
+    if((t[0] == getpid()) && (mark == 0)){//the processes will be created ONLY by the oldest process
       t[i] = fork();
-
       if(t[i] == -1){//optionally -> check if all codes are equal to 0 -> call the success.
 	kill_children(t);
-	printf("The child proccess was not created properly");
+	printf("The child proccess was not created properly\n");
 	exit(1); //error -> exit code 1
       }
       sleep(1);
+    }
+    else if (mark > 0){ //killing already existin children if mark was spotted
+      kill_children(t);
+      return;
     }
   }
   if(t[NUM_CHILD-1] != 0) //means that the last child was born.
     printf("All %d processes were created successfully\n", NUM_CHILD);
 }
 
-int main(){
-  int t[NUM_CHILD]; //the table with all processes ID's
-
-  create_processes(t);
-  
+void synchronize(int* t){
   if(t[0] == getpid()){
     int i = 1;
     while(i != NUM_CHILD){ //counting all children processes terminations.
@@ -64,13 +46,55 @@ int main(){
 	i = i + 1;
     }
     printf("The children are dead.\n");
-    //    kill_children(t);
   }
   else{
     printf("The parent ID: %d\n", getppid());
     sleep(10);
     printf("The child process was completed. Process PID: %d\n", getpid());
   }
-  //while(1);
+}
+
+void ignore(){}
+
+void ignore_all(){
+  for(int i = 0; i < 32; i++){
+    signal(i, ignore);
+  }
+}
+
+void handle_sigint(){
+  printf("The keyboard interrupt was spotted!!!\n");
+  mark = mark + 1;
+}
+
+
+/*
+void handle_sigchld(int sig){
+  printf("STH%d\n", sig); //just for the future purpouse of using handling
+}
+*/
+
+/*
+ * OBJECTIVE:
+ * -SIGNALS
+ * 1) Force ignoring of all signals (signal / (sigaction->restore default handler for SIGCHLD signal))
+ * 2) set your own keyboard interrupt signal handler (SIGINT)
+ * 3) 
+ */
+
+int main(){
+  int t[NUM_CHILD]; //the table with all processes ID's
+  ignore_all();
+  signal(SIGINT, handle_sigint);
+
+  for(int i = 0; i < NUM_CHILD; i++)//fun fact: on fedora the table is not initialized with 0,while on xubuntu it is
+    t[i] = 0;
+
+  create_processes(t);
+  if(mark == 0) //if children are dead, then there is nothing to synchronize
+    synchronize(t);
+  //  if(t[0] == getpid()) //just for the dbg of the interrupt handler
+    //    printf("What's my mark?: %d\n", mark);
+  //    while(mark < 10);
   return 0;
 }
