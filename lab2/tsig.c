bffc50e8386c6e81d4a7eee683206488f262794f
bartoszlomiej <jet.samb@gmail.com>
2021-04-16 19:21:41 +0200

    The major error was fixed

--- lab2/tsig.c
+++ lab2/tsig.c
@@ -10,6 +10,12 @@
 
 int mark = 0;
 
+void ignore(){}
+
+void handle_sigterm(){
+  printf("The children are making revolution. We don't want to die!!!\n\n");
+}
+
 void kill_children(int* t){
   for(int i = 1; i < NUM_CHILD; i++){
     if(t[i] > 0)
@@ -20,18 +26,23 @@ void kill_children(int* t){
 void create_processes(int* t){
   t[0] = getpid(); //pid of the oldest process
   for(int i = 1; i < NUM_CHILD; i++ ){
-    if((t[0] == getpid()) && (mark == 0)){//the processes will be created ONLY by the oldest process
+    if(t[0] == getpid()){//the processes will be created ONLY by the oldest process
       t[i] = fork();
+      
       if(t[i] == -1){//optionally -> check if all codes are equal to 0 -> call the success.
 	kill_children(t);
 	printf("The child proccess was not created properly\n");
 	exit(1); //error -> exit code 1
       }
+      if(getpid() != t[0]){
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTERM, handle_sigterm);
+      }
       sleep(1);
     }
-    else if (mark > 0){ //killing already existin children if mark was spotted
+    if (mark > 0 && getpid() == t[0]){ //killing already existin children if mark was spotted
       kill_children(t);
-      return;
+      printf("The process of making children was interrupted ( ͡° ͟ʖ ͡°)\n" );
     }
   }
   if(t[NUM_CHILD-1] != 0) //means that the last child was born.
@@ -40,10 +51,12 @@ void create_processes(int* t){
 
 void synchronize(int* t){
   if(t[0] == getpid()){
+    signal(SIGCHLD, SIG_DFL);
     int i = 1;
     while(i != NUM_CHILD){ //counting all children processes terminations.
       if(wait(NULL) != -1) //waiting for all children to die.
 	i = i + 1;
+      // printf("O chuj.jpg: %d\n", i);
     }
     printf("The children are dead.\n");
   }
@@ -54,11 +67,15 @@ void synchronize(int* t){
   }
 }
 
-void ignore(){}
-
 void ignore_all(){
   for(int i = 0; i < 32; i++){
-    signal(i, ignore);
+    signal(i, SIG_IGN); //    signal(i, ignore);
+  }
+}
+
+void reset_signals(){
+  for(int i = 0; i < 32; i++){
+    signal(i, SIG_DFL);
   }
 }
 
@@ -74,14 +91,6 @@ void handle_sigchld(int sig){
 }
 */
 
-/*
- * OBJECTIVE:
- * -SIGNALS
- * 1) Force ignoring of all signals (signal / (sigaction->restore default handler for SIGCHLD signal))
- * 2) set your own keyboard interrupt signal handler (SIGINT)
- * 3) 
- */
-
 int main(){
   int t[NUM_CHILD]; //the table with all processes ID's
   ignore_all();
@@ -91,10 +100,14 @@ int main(){
     t[i] = 0;
 
   create_processes(t);
-  if(mark == 0) //if children are dead, then there is nothing to synchronize
-    synchronize(t);
+  
+  //  if(mark == 0) //if children are dead, then there is nothing to synchronize
+  synchronize(t);
   //  if(t[0] == getpid()) //just for the dbg of the interrupt handler
     //    printf("What's my mark?: %d\n", mark);
   //    while(mark < 10);
+  if(getpid() != t[0])
+    reset_signals();
+  //  while(mark < 10);
   return 0;
 }
