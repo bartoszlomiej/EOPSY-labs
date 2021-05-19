4bc80b20414876a6381189db5f6b8998ef4a7716
bartoszlomiej <jet.samb@gmail.com>
2021-05-19 09:55:26 +0200

    prepared all necessary functions, still there are misbegotten semaphores used

--- lab5/philosophers.c
+++ lab5/philosophers.c
@@ -15,7 +15,7 @@ The idea:
 -Each philosopher should be able to eat as much as the rest
 
 In other words:
--Each philosopher is a process (?)
+-Each philosopher is a process
 -The must have functions:
 a) void grab_forks(int left_fork_id);
 b) void put_away_forks(int left_fork_id);
@@ -24,7 +24,19 @@ b) void put_away_forks(int left_fork_id);
 
 //Remember to print on the standard output verbose messages from philosophers
 
-int forks[] = {1, 1, 1, 1, 1};
+#define N 5
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define LEFT (i + N - 1) % N
+#define RIGHT (i + N + 1) % N
+
+struct sembuf sem_op;
+int mutex;
+int s;
+int state[N]; //all states are 0 by default
+
+/*
 
 void eat(){//just for dbg
   printf("Hejo\n");
@@ -34,32 +46,7 @@ void donteat(){//just for dbg
   printf("Glodny\n");
   return;
 }
-/*
-  int sem_key = getsem(int key, int semaphore_number_in_set, int flags_and_permissions)
-  
-  exemplary usage:
-  int sem_set_id;
-  sem_set_id = semget(10, 5, IPC_CREAT | 0600)
-  if(sem_set_id == -1){
-    perror("main: semget");
-    exit(1);
-  }
-//another -> change 10 to IPC_PRIVATE and the semaphore_number_in_set to 1
- */
 
-/*
-//Mutex with  sem_op:
-struct sembuf sem_op;
-sem_op.sem_num = 0;
-sem_op.sem_op = -1; //wait on the semaphore, unless it's value is non-negative
-sem_op.sem_flg = 0;
-
-*Critical Section Here*
-
-sem_op.sem_num = 0;
-sem_op.sem_op = 1; //signal the semaphore that it is free ???
-sem_op.sem_flg = 0;
- */
 void grab_forks(int left_fork_id){
   return;
 }
@@ -67,6 +54,7 @@ void grab_forks(int left_fork_id){
 void put_away_forks(int left_fork_id){
   return;
 }
+*/
 //-----------------------------------------------------------------------------------------------------------------
 int get_semaphore(int id, int n){
   int sem_id = semget(id, n, IPC_CREAT | 0600);
@@ -77,7 +65,7 @@ int get_semaphore(int id, int n){
   return sem_id;
 }
 
-void initialize_semaphore(int sem_set_id){
+void initialize_semaphore(int sem_set_id, int sem_num){ /* Due to the semctl documentation it must be declared */
   union semun {
     int              val;    /* Value for SETVAL */
     struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
@@ -87,31 +75,34 @@ void initialize_semaphore(int sem_set_id){
   };
   union semun sem_val;
   sem_val.val = 1;
-  int ret_val = semctl(sem_set_id, 0, SETVAL, sem_val); //return value from the semctl, simply check.
+  int ret_val = semctl(sem_set_id, sem_num, SETVAL, sem_val); //return value from the semctl, simply check.
   if (ret_val == -1) {
     perror("main: semctl");
     exit(1);
   }
 }
 
-void lock(int sem_set_id, struct sembuf *sem_op){
+void lock(int sem_set_id, int sem_num){
   /*struct sembuf semop;*/
-  (&(*sem_op))->sem_num = 0;
-  (&(*sem_op))->sem_op = -1;
-  (&(*sem_op))->sem_flg = 0;
-  semop(sem_set_id, sem_op, 1);
+  sem_op.sem_num = sem_num;
+  sem_op.sem_op = -1;
+  sem_op.sem_flg = 0;
+  semop(sem_set_id, &sem_op, 1);
 }
 
-void unlock(int sem_set_id, struct sembuf *sem_op){
-    sem_op->sem_num = 0;
-    sem_op->sem_op = 1;
-    sem_op->sem_flg = 0;
-    semop(sem_set_id, sem_op, 1);
+void unlock(int sem_set_id, int sem_num){
+  sem_op.sem_num = sem_num; //the semaphore number in the set
+  sem_op.sem_op = 1;
+  sem_op.sem_flg = 0;
+  semop(sem_set_id, &sem_op, 1);
 }
 //-----------------------------------------------------------------------------------------------------------------
 //There are 5 philosophers -> from 0 to 4
-void philosopher(int number){
+//void philosopher(int number){
+  
+  /*
   printf("My pid: %d, my count: %d\n", getpid(), number);
+
   if(number == 0){ //border case
     if((forks[0] == 1) && (forks[4] == 1))
       eat(); //just for dbg
@@ -128,12 +119,61 @@ void philosopher(int number){
       eat(); //just for dbg
     else
       donteat();
+  */
+//}
+
+//-----------------------------------------------------------------------------------------------------------------
+void eat(){
+  printf("eating Philosopher: %d\n", getpid());
 }
 
+void think(){
+  printf("//thinking Philosopher: %d\n", getpid());
+}
+//-----------------------------------------------------------------------------------------------------------------
+
+void test(i){
+  if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
+    state[i] = EATING;
+    unlock(s, i);
+  }
+}
+
+void put_away_forks(i){
+  lock(mutex, 0);
+  state[i] = THINKING;
+  test(LEFT);
+  test(RIGHT);
+  unlock(mutex, 0);
+}
+
+void grab_forks(int i){
+  lock(mutex, 0);
+  state[i] = HUNGRY;
+  test(i);
+  unlock(mutex, 0);
+  lock(s, i);
+}
+
+void philosopher(int i){
+  int j = 0;
+  while(j < 2){
+    think( );
+    grab_forks(i);
+    eat( );
+    put_away_forks(i);
+    j++;
+  }
+}
+//-----------------------------------------------------------------------------------------------------------------
 void create_philosophers(){
-  struct sembuf sem_op;
-  int a = get_semaphore(100, 1);
-  initialize_semaphore(a);
+  mutex = get_semaphore(100, 1); //mutex creation
+  s = get_semaphore(200, 5); //the set of 5 semaphores
+
+  initialize_semaphore(mutex, 0);
+  for(int i = 0; i < N; i++){
+    initialize_semaphore(s, i);
+  }
   
   int t[5];
   int count = -1;
@@ -146,24 +186,22 @@ void create_philosophers(){
   }
   if(t[0] == getpid()){
     count = count + 1; //the first philosopher number incrementation
-    sleep(5);
+    //    sleep(5);
   }
+  /*
   if(t[0] != getpid()){
     //      lock(a, &sem_op);
       sleep(1);
       //      unlock(a, &sem_op);
   }
-  
+  */
   philosopher(count);
 }
 //int get_semaphore(int id, int n){
 //void initialize_semaphore(int sem_set_id){
-//void lock(int sem_set_id, struct sembuf *sem_op){
-//void unlock(int sem_set_id, struct sembuf *sem_op){
-int main(){
-
-
+//void lock(int sem_set_id, int num){
+//void unlock(int sem_set_id, int num){
+int main(){  
   create_philosophers();
-  //  unlock(a, &sem_op);
   return 0;
 }
