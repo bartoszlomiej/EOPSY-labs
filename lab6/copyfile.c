81ff6b43aae15146c8cbe4668576b8ec57147815
bartoszlomiej <jet.samb@gmail.com>
2021-05-25 12:25:13 +0200

    The usage of getopt was done

--- lab6/copyfile.c
+++ lab6/copyfile.c
@@ -8,30 +8,26 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-  if (argc == 2){
-    if(!strcmp(argv[1], "-h")){
-      printf("Help should be printed\n");
-    }
-    else{
-      printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
-      return -1; //if there is wrong input - exit
-    }
-  }
-  else if(argc == 3){
-    printf("it is assumed that two files names are here\n");
-  }
-  else if(argc == 4){
-    if(!strcmp(argv[1], "-m")){
-      printf("The -m option should be used\n");
-    }
-    else{
-      return -1; //if there is wrong input - exit
-      printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
+  int c;
+  int i = 0;
+  while(( c = getopt(argc, argv, ":mh" )) != -1 ){ //parsing flags only
+    i = i + 1;
+    switch(c){
+    case 'm':
+      printf("-m,   ");
+      break;
+    case 'h':
+      printf("-h,   ");
+      return 0; //prevent to further execution of the program if help was set
+      break;
+    case '?':
+      printf("wrong parameters were given\n");
+      printf("Type -h flag to show the help\n");
+      return -1;
     }
   }
-  else{
-    printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
-    return -1;
+  for(; optind < argc; optind++){ //parsing later arguments
+    printf("The next arguments: %s\n:", argv[optind]); 
   }
   return 0;
 }
