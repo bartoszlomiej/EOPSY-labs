8ef756447acb40bbb9b3c781e7acc3c8dddd1e56
bartoszlomiej <jet.samb@gmail.com>
2021-05-25 11:44:18 +0200

    the basic skelet of the program

--- lab6/copyfile.c
+++ lab6/copyfile.c
@@ -3,7 +3,35 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv){
+  if (argc == 2){
+    if(!strcmp(argv[1], "-h")){
+      printf("Help should be printed\n");
+    }
+    else{
+      printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
+      return -1; //if there is wrong input - exit
+    }
+  }
+  else if(argc == 3){
+    printf("it is assumed that two files names are here\n");
+  }
+  else if(argc == 4){
+    if(!strcmp(argv[1], "-m")){
+      printf("The -m option should be used\n");
+    }
+    else{
+      return -1; //if there is wrong input - exit
+      printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
+    }
+  }
+  else{
+    printf("Wrong input was given. Type './copyfiles -h' to check the proper syntax\n");
+    return -1;
+  }
   return 0;
 }
